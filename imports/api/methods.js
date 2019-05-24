95fb246dfb508eb57a1fbb88f7d0a8920b8a2a20
Bakhodir Annaev <bakhodir.annaev.firefly@gmail.com>
2019-05-24 18:36:34 +0500

    preparing methods

---
+++ imports/api/methods.js
@@ -0,0 +1,38 @@
+import { Meteor } from 'meteor/meteor';
+import { HTTP } from 'meteor/http';
+
+Meteor.methods({
+  'Shows.get': (page = 1, limit = 20) => {
+    const result = HTTP.get(`https://api-staging.trakt.tv/shows/popular?page=${page}&limit=${limit}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'trakt-api-version': 2,
+        'trakt-api-key': 'f59c83906e11f6219cc22ecba45b736fcfb11c9ddef9f51352ebdd9aba98de6c',
+      },
+    });
+
+    return result.data;
+  },
+  'Shows.search': query => {
+    const result = HTTP.get(`https://api.trakt.tv/search/movie?query=${query}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'trakt-api-version': 2,
+        'trakt-api-key': 'f59c83906e11f6219cc22ecba45b736fcfb11c9ddef9f51352ebdd9aba98de6c',
+      },
+    });
+
+    return result.data;
+  },
+  'Show.getDetail': id => {
+    const result = HTTP.get(`https://api.trakt.tv/shows/${id}?extended=full`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'trakt-api-version': 2,
+        'trakt-api-key': 'f59c83906e11f6219cc22ecba45b736fcfb11c9ddef9f51352ebdd9aba98de6c',
+      },
+    });
+
+    return result.data;
+  },
+});
--- imports/api/shows.js
+++ imports/api/shows.js
@@ -1,4 +1,3 @@
-import { HTTP } from 'meteor/http';
 import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
 
@@ -7,19 +6,5 @@ const Shows = new Mongo.Collection('shows');
 if (Meteor.isServer) {
   Meteor.publish('shows', () => Shows.find());
 }
-
-Meteor.methods({
-  ShowsGet(page = 1, limit = 20) {
-    const result = HTTP.get(`https://api-staging.trakt.tv/shows/popular?page=${page}&limit=${limit}`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'trakt-api-version': 2,
-        'trakt-api-key': 'f59c83906e11f6219cc22ecba45b736fcfb11c9ddef9f51352ebdd9aba98de6c',
-      },
-    });
-
-    return result.data;
-  },
-});
 
 export default Shows;
